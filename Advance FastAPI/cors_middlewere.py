76303a260f55265b7819bafc16452dc839ba54a4
Priyanshu Tiwari <priyanshutiwari8318.2@gmail.com>
2026-08-17 23:48:46 +0530

    Date 17/8/2026

--- Advance FastAPI/cors_middlewere.py
+++ Advance FastAPI/cors_middlewere.py
@@ -3,11 +3,4 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['https:/my-frontednd.com','https:/locolhost.com'],
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
-)
 
