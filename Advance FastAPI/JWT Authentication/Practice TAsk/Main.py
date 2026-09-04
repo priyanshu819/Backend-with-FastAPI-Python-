0d89ee7419b796d6de51b38d573d73d3ccb086cd
Priyanshu Tiwari <priyanshutiwari8318.2@gmail.com>
2026-09-04 19:12:38 +0530

    Commit Now

--- Advance FastAPI/JWT Authentication/Practice TAsk/Main.py
+++ Advance FastAPI/JWT Authentication/Practice TAsk/Main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI,HTTPException
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel,EmailStr,field_validator,Field
 from Security import hash_pas ,verify_password
+import re
 
 app=FastAPI()
 
 # Create A PydanticMOdel for Registration
 class User(BaseModel):
-    username:str
+    username:str=Field(...,  description="use only a-z, 0-9 and underscore _ ") 
+    fullname:str
     email:EmailStr
     password:str
 
@@ -22,16 +24,30 @@ def home():
 #create Register Api
 @app.post('/register')
 def register(user:User):
+
+    # create condition for uniqueness of the email and username
+    if user.username in fake_db:
+        raise HTTPException(status_code=400, detail="username alredy exist")
+    for existEmail in fake_db.values():
+        if existEmail['email']==user.email:
+            raise HTTPException(status_code=400, detail="email alredy exist")
+
+    # set the credintials of the username
+    pattern=r"^[a-z0-9_]+$"
+    if not re.match(pattern,user.username) or not (3<=len(user.username)<=20):
+        raise HTTPException(status_code=422, detail="use only a-z, 0-9 and underscore _ in username")
+
     hashed_pass=hash_pas(user.password)
     fake_db[user.username]={
         "username":user.username,
+        "fullname":user.fullname,
         "email":user.email,
         "password":hashed_pass
     }
     
     return {'message':'User Got Regesterd SucssesFully !'}
 
-# Create A PydanticMOdel for Registration
+# Create A PydanticMOdel for login
 class User(BaseModel):
     
     username:str
@@ -49,4 +65,4 @@ def login(user:User):
     check_password=verify_password(user.password,stored_user['password'])
     if not check_password:
         raise HTTPException(status_code=401, detail="Invalid Password !")
-    return f"Hey {user.username} Login SuccessFully"
+    return f"Hey {stored_user['fullname']} Login SuccessFully"
--- Advance FastAPI/JWT Authentication/Practice TAsk/temp.py
+++ Advance FastAPI/JWT Authentication/Practice TAsk/temp.py
@@ -1,23 +1,32 @@
-import bcrypt
+# import bcrypt
 
 
-username=input("Enter Username: ")
-password=input("Enter Password: ")
+# username=input("Enter Username: ")
+# password=input("Enter Password: ")
 
 
-def hash_pas(password:str):
-    return bcrypt.hashpw(
-        password.encode('utf-8'),
-        bcrypt.gensalt()
-    ).decode('utf-8')
+# def hash_pas(password:str):
+#     return bcrypt.hashpw(
+#         password.encode('utf-8'),
+#         bcrypt.gensalt()
+#     ).decode('utf-8')
 
-hashed_pass=hash_pas(password)
+# hashed_pass=hash_pas(password)
 
-login={
-    'username':username,
-    'pass':hash_pas
+# login={
+#     'username':username,
+#     'pass':hash_pas
+# }
+
+# print(login)
+# print('----------------------------------------')
+# print(f"  Hey {login['username'][0].upper()}{login['username'][1:len(login['username'])]} Welcome !")
+# print('----------------------------------------')
+
+
+fake_db={
+    "username":"ram",
+    "email":"priya@gmail.com"
 }
 
-print(login)
-
-print(f"Hey {login['username']} Welcom !")
+print(fake_db.values())
